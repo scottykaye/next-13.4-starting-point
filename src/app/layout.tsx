500211c3231082c37a308863a16f907a00d60923
Scott Kaye <skaye@wayfair.com>
2023-07-29 12:53:21 -0400

    Just commit a new starting point

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ weight: '400', subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'App Title',
+  description: 'App description',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en-US">
+      <head>
+        <title>Alpha Centauri</title>
+        <meta name="color-scheme" content="light dark" />
+      </head>
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
